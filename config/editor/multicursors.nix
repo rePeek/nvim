8073e2efb15d1862e9c241a06c945c222986cd32
rePeek <senxlin@gmail.com>
2026-08-15 11:40:06 +0800

    fix: add multicursors keymap for entering multicursor mode
    feat: add Up/Down arrow keys for multicursor create up/down

--- config/editor/multicursors.nix
+++ config/editor/multicursors.nix
@@ -2,5 +2,24 @@
   # ── Multicursors ──
   plugins.multicursors = {
     enable = true;
+    settings.normal_keys = {
+      "<Down>" = {
+        method = { __raw = "require('multicursors.normal_mode').create_down"; };
+        opts.desc = "Create down";
+      };
+      "<Up>" = {
+        method = { __raw = "require('multicursors.normal_mode').create_up"; };
+        opts.desc = "Create up";
+      };
+    };
   };
+
+  keymaps = [
+    {
+      mode = [ "n" "v" ];
+      key = "<leader>m";
+      action = "<cmd>MCstart<cr>";
+      options.desc = "Start multicursor mode";
+    }
+  ];
 }
