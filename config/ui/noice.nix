a570e4c054f7824add24ed9a7404c85a5d174fa0
rePeek <senxlin@gmail.com>
2026-08-21 19:32:37 +0800

    feat: align Noice cmdline with input layout

--- config/ui/noice.nix
+++ config/ui/noice.nix
@@ -12,6 +12,38 @@
           "cmp.entry.get_documentation" = true;
         };
       };
+      views = {
+        cmdline_popup = {
+          position = {
+            # NUI's complex border shifts the outer frame up by one row.
+            row = 3;
+            col = "50%";
+          };
+          size = {
+            width = 60;
+            height = "auto";
+          };
+          border.padding = [
+            0
+            0
+          ];
+        };
+        cmdline_popupmenu = {
+          position = {
+            row = 6;
+            col = "50%";
+          };
+          size = {
+            width = 60;
+            height = "auto";
+            max_height = 15;
+          };
+          border.padding = [
+            0
+            0
+          ];
+        };
+      };
       routes = [
         {
           filter = {
@@ -26,7 +58,7 @@
         }
       ];
       presets = {
-        bottom_search = true;
+        bottom_search = false;
         command_palette = true;
         long_message_to_split = true;
         lsp_doc_border = true;
